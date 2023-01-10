019d6e51e2e1706409599ddde7464c528fa1f062
Mauro Esposito <65233437+mauroesposito90@users.noreply.github.com>
2023-01-10 12:26:06 +0100

    Add files via upload

--- Fastq.R
+++ Fastq.R
@@ -1,3 +1,20 @@
+# Copyrigth (C) 2023 Mauro Esposito
+
+# This file is part of Rindalyzer.
+
+# Rindalyzer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Rindalyzer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Rindalyzer.  If not, see <http://www.gnu.org/licenses/>.
+
 ## The function of the module fastq is to create the FASTQ from the consensus BAM
 # @param consensusBAM Path to the consensus BAM
 # @param nThreads Number of threads to use
--- Rindalyzer.R
+++ Rindalyzer.R
@@ -1,5 +1,22 @@
 #!/usr/bin/env Rscript
 
+# Copyrigth (C) 2023 Mauro Esposito
+
+# This file is part of Rindalyzer.
+
+# Rindalyzer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Rindalyzer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Rindalyzer.  If not, see <http://www.gnu.org/licenses/>.
+
 ## The Rindalyzer tool analyzes the autonomous transcription of TEs
 # @param FASTQ_1 Link to FASTQ files
 # @param FASTQ_2 Link to FASTQ files
